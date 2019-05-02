e7a0c44e05dd9e9fab16a06e270cd8cfffcf64b8
Isaac Johnson <45494522+geohci@users.noreply.github.com>
2019-05-02 09:26:14 -0500

    default ot best topic

--- switches_by_category.py
+++ switches_by_category.py
@@ -38,67 +38,69 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--ores_output")
     parser.add_argument("--switches_tsv")
+    parser.add_argument("--approach", default='best', help="How to count topics: one of naive, rand, all, best.")
     args = parser.parse_args()
 
-    for approach in [('naive', get_pred_topic_naive),
-                     ('rand', get_pred_topic_rand),
-                     ('all', get_pred_topic_all),
-                     ('best', get_pred_topic_best)]:
-        print("==== {0} ====".format(approach[0]))
-        get_pred_topic = approach[1]
-        qid_to_topic = {}
-        with open(args.ores_output, 'r') as fin:
-            for line in fin:
-                record = json.loads(line)
-                qid = record['qid']
-                topic = get_pred_topic(record)
-                qid_to_topic[qid] = topic
+    approaches = {'naive': get_pred_topic_naive,
+                  'rand': get_pred_topic_rand,
+                  'all': get_pred_topic_all,
+                  'best': get_pred_topic_best}
+    get_pred_topic = approaches[args.approach]
 
-        no_topic = 0
-        non_switch = 0
-        switch_topics = {}
-        with open(args.switches_tsv, 'r') as fin:
-            header = ['switch', 'country', 'qid', 'title', 'datetime', 'usertype']
-            switch_idx = header.index('switch')
-            qid_idx = header.index('qid')
-            for line in fin:
-                line = line.strip().split('\t')
-                if line[switch_idx] == 'N\A' or line[switch_idx] == 'N/A':
-                    non_switch += 1
-                    continue
-                qid = line[qid_idx]
-                try:
-                    topic = qid_to_topic[qid]
-                except KeyError:
-                    no_topic += 1
-                    continue
-                if approach[0] == 'all':
-                    for t in topic:
-                        switch_topics[t] = switch_topics.get(t, 0) + 1
-                else:
-                    switch_topics[topic] = switch_topics.get(topic, 0) + 1
-        total_switches = sum(switch_topics.values())
-        print("{0} different topics, {1} switches w/ topics. {2} switches w/o topics. {3} non-switches.".format(
-            len(switch_topics), total_switches, no_topic, non_switch))
+    print("==== {0} ====".format(args.approach))
+    qid_to_topic = {}
+    with open(args.ores_output, 'r') as fin:
+        for line in fin:
+            record = json.loads(line)
+            qid = record['qid']
+            topic = get_pred_topic(record)
+            qid_to_topic[qid] = topic
 
-        topicdf = pd.DataFrame([(topic, count) for topic, count in switch_topics.items()], columns=['topic', 'count'])
-        topicdf['proportion'] = topicdf['count'].apply(lambda x: x / total_switches)
-
-        topicdf.sort_values(by='proportion', ascending=False, inplace=True)
-        print(topicdf)
-
-        toptopics = {}
-        for t in switch_topics:
-            if t:
-                toptopic = t.split('.')[0]
+    no_topic = 0
+    non_switch = 0
+    switch_topics = {}
+    with open(args.switches_tsv, 'r') as fin:
+        header = ['switch', 'country', 'qid', 'title', 'datetime', 'usertype']
+        switch_idx = header.index('switch')
+        qid_idx = header.index('qid')
+        for line in fin:
+            line = line.strip().split('\t')
+            if line[switch_idx] == 'N\A' or line[switch_idx] == 'N/A':
+                non_switch += 1
+                continue
+            qid = line[qid_idx]
+            try:
+                topic = qid_to_topic[qid]
+            except KeyError:
+                no_topic += 1
+                continue
+            if args.approach == 'all':
+                for t in topic:
+                    switch_topics[t] = switch_topics.get(t, 0) + 1
             else:
-                toptopic = 'None'
-            toptopics[toptopic] = toptopics.get(toptopic, 0) + switch_topics[t]
-        toptopicdf = pd.DataFrame([(topic, count) for topic, count in toptopics.items()], columns=['topic', 'count'])
-        toptopicdf['proportion'] = toptopicdf['count'].apply(lambda x: x / total_switches)
+                switch_topics[topic] = switch_topics.get(topic, 0) + 1
+    total_switches = sum(switch_topics.values())
+    print("{0} different topics, {1} switches w/ topics. {2} switches w/o topics. {3} non-switches.".format(
+        len(switch_topics), total_switches, no_topic, non_switch))
 
-        toptopicdf.sort_values(by='proportion', ascending=False, inplace=True)
-        print(toptopicdf)
+    topicdf = pd.DataFrame([(topic, count) for topic, count in switch_topics.items()], columns=['topic', 'count'])
+    topicdf['proportion'] = topicdf['count'].apply(lambda x: x / total_switches)
+
+    topicdf.sort_values(by='proportion', ascending=False, inplace=True)
+    print(topicdf)
+
+    toptopics = {}
+    for t in switch_topics:
+        if t:
+            toptopic = t.split('.')[0]
+        else:
+            toptopic = 'None'
+        toptopics[toptopic] = toptopics.get(toptopic, 0) + switch_topics[t]
+    toptopicdf = pd.DataFrame([(topic, count) for topic, count in toptopics.items()], columns=['topic', 'count'])
+    toptopicdf['proportion'] = toptopicdf['count'].apply(lambda x: x / total_switches)
+
+    toptopicdf.sort_values(by='proportion', ascending=False, inplace=True)
+    print(toptopicdf)
 
 if __name__ == "__main__":
     main()
