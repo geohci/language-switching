371cd87f08688303ebbbb35f5ab7098b3e9d5bfa
Isaac Johnson <45494522+geohci@users.noreply.github.com>
2019-05-02 10:45:38 -0500

    include non-switches as comparison

--- switches_by_category.py
+++ switches_by_category.py
@@ -4,6 +4,8 @@ import json
 import numpy as np
 import pandas as pd
 
+NON_SWITCHES = ('N\A', 'N/A')
+
 def get_pred_topic_naive(input_json):
     try:
         topic = input_json['score']['drafttopic']['score']['prediction'][0]
@@ -33,7 +35,6 @@ def get_pred_topic_best(input_json):
         best = None
     return best
 
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--ores_output")
@@ -56,50 +57,70 @@ def main():
             topic = get_pred_topic(record)
             qid_to_topic[qid] = topic
 
-    no_topic = 0
-    non_switch = 0
+    s_no_topic = 0
+    n_no_topic = 0
     switch_topics = {}
+    noswitch_topics = {}
     with open(args.switches_tsv, 'r') as fin:
         header = ['switch', 'country', 'qid', 'title', 'datetime', 'usertype']
         switch_idx = header.index('switch')
         qid_idx = header.index('qid')
         for line in fin:
             line = line.strip().split('\t')
-            if line[switch_idx] == 'N\A' or line[switch_idx] == 'N/A':
-                non_switch += 1
-                continue
             qid = line[qid_idx]
             try:
                 topic = qid_to_topic[qid]
             except KeyError:
-                no_topic += 1
+                if line[switch_idx] in NON_SWITCHES:
+                    n_no_topic += 1
+                else:
+                    s_no_topic += 1
                 continue
             if args.approach == 'all':
                 for t in topic:
-                    switch_topics[t] = switch_topics.get(t, 0) + 1
+                    if line[switch_idx] in NON_SWITCHES:
+                        noswitch_topics[t] = noswitch_topics.get(t, 0) + 1
+                    else:
+                        switch_topics[t] = switch_topics.get(t, 0) + 1
             else:
-                switch_topics[topic] = switch_topics.get(topic, 0) + 1
+                if line[switch_idx] in NON_SWITCHES:
+                    noswitch_topics[topic] = noswitch_topics.get(topic, 0) + 1
+                else:
+                    switch_topics[topic] = switch_topics.get(topic, 0) + 1
     total_switches = sum(switch_topics.values())
-    print("{0} different topics, {1} switches w/ topics. {2} switches w/o topics. {3} non-switches.".format(
-        len(switch_topics), total_switches, no_topic, non_switch))
+    total_nonswitches = sum(noswitch_topics.values())
+    print("    Switches:\t{0} different topics;\t{1} w/ topics;\t{2} w/o topics.".format(
+        len(switch_topics), total_switches, s_no_topic))
+    print("Non-switches:\t{0} different topics;\t{1} w/ topics;\t{2} w/o topics.".format(
+        len(noswitch_topics), total_nonswitches, n_no_topic))
 
-    topicdf = pd.DataFrame([(topic, count) for topic, count in switch_topics.items()], columns=['topic', 'count'])
-    topicdf['proportion'] = topicdf['count'].apply(lambda x: x / total_switches)
+    all_topics = set(switch_topics.keys())
+    all_topics.update(noswitch_topics.keys())
+    topicdf = pd.DataFrame([
+        (topic, switch_topics.get(topic, 0), noswitch_topics.get(topic, 0)) for topic in all_topics],
+        columns=['topic', 'switch_count', 'nonswitch_count'])
+    topicdf['switch_proportion'] = topicdf['switch_count'].apply(lambda x: x / total_switches)
+    topicdf['nonswitch_proportion'] = topicdf['nonswitch_count'].apply(lambda x: x / total_nonswitches)
 
-    topicdf.sort_values(by='proportion', ascending=False, inplace=True)
+    topicdf.sort_values(by='switch_proportion', ascending=False, inplace=True)
     print(topicdf)
 
-    toptopics = {}
-    for t in switch_topics:
+    s_toptopics = {}
+    n_toptopics = {}
+    for t in all_topics:
         if t:
             toptopic = t.split('.')[0]
         else:
             toptopic = 'None'
-        toptopics[toptopic] = toptopics.get(toptopic, 0) + switch_topics[t]
-    toptopicdf = pd.DataFrame([(topic, count) for topic, count in toptopics.items()], columns=['topic', 'count'])
-    toptopicdf['proportion'] = toptopicdf['count'].apply(lambda x: x / total_switches)
+        s_toptopics[toptopic] = s_toptopics.get(toptopic, 0) + switch_topics.get(t, 0)
+        n_toptopics[toptopic] = n_toptopics.get(n_toptopics, 0) + noswitch_topics.get(t, 0)
+    toptopicdf = pd.DataFrame([
+        (topic, s_toptopics[topic], n_toptopics[topic]) for topic in s_toptopics],
+        columns=['topic', 'switch_count', 'nonswitch_count'])
+    toptopicdf['switch_proportion'] = toptopicdf['switch_count'].apply(lambda x: x / total_switches)
+    toptopicdf['nonswitch_proportion'] = toptopicdf['nonswitch_count'].apply(lambda x: x / total_nonswitches)
 
-    toptopicdf.sort_values(by='proportion', ascending=False, inplace=True)
+    toptopicdf.sort_values(by='switch_proportion', ascending=False, inplace=True)
     print(toptopicdf)
 
 if __name__ == "__main__":
